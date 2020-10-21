68f2249a95abe1787b0dd852ec129b5483c834f4
PranayRathi <40464551+PranayRathi@users.noreply.github.com>
2020-10-21 21:08:45 +0530

    change2

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,30 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import "../styles/App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.shoot = this.shoot.bind(this);
   }
 
   change() {
     return (
-      <p id="para">
-        You have to make a button that on click will render a paragraph tag,
-        Button should contain id click and para(when rendered)should have id
-        para.
-      </p>
+      <>
+        <p id="para">
+          You have to make a button that on click will render a paragraph tag,
+          Button should contain id click and para(when rendered)should have id
+          para.
+        </p>
+      </>
     );
   }
   render() {
     return (
       <div id="main">
-        <button id="click" onClick={this.change}></button>
+        <button id="click" onClick={this.change}>
+          click
+        </button>
       </div>
     );
   }
