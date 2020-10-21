1c0f10b39f8e2845ead6049daa590b9928e5dce1
PranayRathi <40464551+PranayRathi@users.noreply.github.com>
2020-10-21 22:00:07 +0530

    change5

--- src/components/App.js
+++ src/components/App.js
@@ -8,12 +8,11 @@ class App extends Component {
     this.change = this.change.bind(this);
   }
 
-  change(props) {
+  change() {
     return (
       <p id="para">
-        You have to make a button that on click will render a paragraph tag,
-        Button should contain id click and para(when rendered)should have id
-        para.
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me
+        so happy
       </p>
     );
   }
