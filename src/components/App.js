a4f6570de8ec27f733d85c74fb637716d2770c39
PranayRathi <40464551+PranayRathi@users.noreply.github.com>
2020-10-21 20:57:44 +0530

    change1

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,27 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+  change() {
+    return (
+      <p id="para">
+        You have to make a button that on click will render a paragraph tag,
+        Button should contain id click and para(when rendered)should have id
+        para.
+      </p>
+    );
+  }
+  render() {
+    return (
+      <div id="main">
+        <button id="click" onClick={this.change}></button>
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
